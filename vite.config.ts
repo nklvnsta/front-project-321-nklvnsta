5b6a596faa3247e2515ed2da43d02004bf244560
NikolaevaAnastasia <nastanikol10105@gmail.com>
2024-06-01 14:20:27 +0300

    568

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"front-project-321-nklvnsta",
   plugins: [react(), VitePWA({
     registerType: 'prompt', 
     includeAssets: ['favicon.ico', 'logo.png', 'logo.svg'],
